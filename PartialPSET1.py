d1aa212f1f00f0e52c4a19fed2d147a6daf41697
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-04-07 13:41:14 -0400

    fixing

--- PartialPSET1.py
+++ PartialPSET1.py
@@ -1,16 +1,19 @@
+# https://cis192.github.io/assignments/
+
 def substrings(seq):
+    sequence = str(seq)
 
     substring_list = set()
 
-    for i in range(float(len(seq))):
-        for j in range(float(len(seq)):
-            substring = seq[j:len(seq):i]
-            substring_list.add(substring)
+    for i in range(float(len(sequence))):
+        # for j in range(float(len(sequence)):
+        substring = sequence[j:len(sequence):i]
+        substring_list.add(substring)
 
-    return(substring_list)
+    return substring_list
+
 
 def all_factors(num):
-
     factors = set()
 
     for counter in range(1, num):
@@ -19,32 +22,23 @@ def all_factors(num):
             factors.add(num)
     counter += 1
 
-    return(factors)
-   
-"""
+    return (factors)
 
-all_factors = set()
-
-for counter in range(n):
-    if counter % n == 0
-        all_factors.add(counter)
-
-return(all_factors)//
 
 def all_but_last(seq):
+    sequence = seq[0:(len(seq) - 1)]
+    return sequence
 
-    sequence = seq[0:(len(seq)-1)]
 
-    return(sequence)
-    
-    def alphabet_construct(seq, alphabet):
+
+def alphabet_construct(seq, alphabet):
 
     return True if seq in (set of combined strings in list comprehension)
 
     nested for loop
-    
-    """
-    
+
+
+        """
 def every_other(seq):
 
     sliced = seq[0:len(seq):2]
@@ -63,3 +57,4 @@ def get_student_avg(student, gradebook_dict):
         
 def many_any(lst, k):
     return True for x in lst if x > k        
+"""
